9518906f1ee35af794dfb8097342ab8ee5082b73
Gold122 <kontakt@goldproject.eu>
2018-04-26 20:57:44 +0200

    Update sql

--- gbot.sql
+++ gbot.sql
@@ -1,42 +1,88 @@
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Zrzut struktury tabela ts_gbot.newuserToday
-CREATE TABLE IF NOT EXISTS `newuserToday` (
+--
+-- Table structure for table `channelPrivate`
+--
+
+DROP TABLE IF EXISTS `channelPrivate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `channelPrivate` (
+  `dbid` int(11) NOT NULL,
+  `channels` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`dbid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `newuserToday`
+--
+
+DROP TABLE IF EXISTS `newuserToday`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `newuserToday` (
   `dbid` int(11) NOT NULL,
   `client_nickname` varchar(200) NOT NULL,
   `client_unique_identifier` varchar(200) NOT NULL,
   `time` int(11) DEFAULT NULL,
   PRIMARY KEY (`dbid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
--- Zrzut struktury tabela ts_gbot.record
-CREATE TABLE IF NOT EXISTS `record` (
+--
+-- Table structure for table `record`
+--
+
+DROP TABLE IF EXISTS `record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `record` (
   `uid` varchar(150) NOT NULL,
   `online` int(11) DEFAULT NULL,
   `time` int(11) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `record` (`online`, `time`) VALUES ('0', '0');
 
--- Data exporting was unselected.
--- Zrzut struktury tabela ts_gbot.static
-CREATE TABLE IF NOT EXISTS `static` (
+--
+-- Table structure for table `static`
+--
+
+DROP TABLE IF EXISTS `static`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `static` (
   `id` int(11) NOT NULL,
   `nick` varchar(200) NOT NULL,
   `mem` varchar(200) NOT NULL,
   `online` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
--- Zrzut struktury tabela ts_gbot.tops
-CREATE TABLE IF NOT EXISTS `tops` (
+
+--
+-- Table structure for table `tops`
+--
+
+DROP TABLE IF EXISTS `tops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tops` (
   `client_database_id` int(100) NOT NULL,
   `client_unique_identifier` varchar(50) NOT NULL,
   `client_nickname` varchar(100) DEFAULT NULL,
@@ -47,7 +93,13 @@ CREATE TABLE IF NOT EXISTS `tops` (
   PRIMARY KEY (`client_database_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
